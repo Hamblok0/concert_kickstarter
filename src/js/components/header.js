791406aa4d30507505449aba01780520a41f2233
Mary Ellen Klein <maryellenklein@gmail.com>
2015-12-10 10:16:04 -0600

    adding login and register html

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -5,10 +5,10 @@ export default (props) => (
   <header>
     <a href="#"><img src="../images/logo.png" alt="Logo" width="80px"/></a>
     <nav>
-      <a className="gitBtn" href="#band/1/edit">Start A Gig</a>
-      <a className="discoverBtn" href="#">Discover</a>
-      <a className="registerBtn" href="#register">Sign up</a>
-      <a className="loginBtn" href="#login">Log in</a>
+      <a className="gitBtn btn" href="#band/1/edit">Start A Gig</a>
+      <a className="discoverBtn btn" href="#">Discover</a>
+      <a className="registerBtn guestBtn" href="#register">Sign up</a>
+      <a className="loginBtn guestBtn" href="#login">Log in</a>
     </nav>
   </header>
 )
--- src/js/components/register.js
+++ src/js/components/register.js
@@ -3,9 +3,13 @@ import React from 'react';
 class Register extends React.Component {
   render () {
     return(
-      <section>
-        <p>Hello I am register!</p>
-      </section>
+      <form>
+        <input type="text" className="email" placeholder="Email" ref="email" />
+        <input type="password" className="password" placeholder="Password" ref="pwd" />
+        <input type="password" className="password" placeholder="Confirm password" ref="pwd1" />
+        <input type="submit" className="registerBtn" value="Sign up" />
+        <p className="instructions">Already have an account? <a href="#login" className="formLink">Login »</a></p>
+      </form>
     )
   }
 }
