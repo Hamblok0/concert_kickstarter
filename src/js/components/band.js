9a47df66febb0891d3135c9986ea41d05ac09f9b
Joseph Cooper <hambloko@gmail.com>
2015-12-10 11:28:05 -0600

    added framework for the band view

--- src/js/components/band.js
+++ src/js/components/band.js
@@ -7,62 +7,59 @@ class Band extends React.Component {
       html = this.props.children
     } else {
       html = (
-        <div>
-          <article className="cover">
-            <div>
-              <h1>Your Band | Your City</h1>
-              <button name="Edit Profile"></button>
-            </div>
-            <img src="http://antikhobi.com/wp-content/uploads/2014/07/img_placeholder21.png"/>
-          </article>
-          <article className="bandInfo">
-            <div className="soundCloud"><iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/231073377&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe></div>
-            <div className="youtube"><iframe width="420" height="315" src="https://www.youtube.com/embed/XFkzRNyygfk" frameborder="0" allowfullscreen></iframe></div>
 
-            <section className="locGenBox">
-              <div className="location">
-                <i className="fa fa-map-marker"></i>
-                <h3>Nashville</h3>
+          <div>
+            <article className="cover">
+              <div>
+                <h1>Your Band | Your City</h1>
+                <button name="Edit Profile"></button>
               </div>
-              <div className="genre">
-                <i className="fa fa-tag"></i>
-                <h3>Post-Slow-Noise-Core-Wave</h3>
-              </div>
-            </section>
+              <img src="http://antikhobi.com/wp-content/uploads/2014/07/img_placeholder21.png"/>
+            </article>
+            <article className="bandInfo">
+              <div className="soundCloud"><iframe width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/231073377&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe></div>
+              <div className="youtube"><iframe width="420" height="315" src="https://www.youtube.com/embed/XFkzRNyygfk" frameborder="0" allowfullscreen></iframe></div>
 
-            <section className="fundInfoBox">
-              <span className="fanSpan">
-                <h3>Over 9000</h3>
-                <p>True Fans</p>
-              </span>
-              <span className="fundSpan">
-                <h3>£12</h3>
-                <p>pledged of £1000</p>
-              </span>
-              <span className="daysSpan">
-                <h3>11</h3>
-                <p>days to go</p>
-              </span>
-              <button className="bringBut" name="Bring This Band"></button>
-            </section>
+              <section className="locGenBox">
+                <div className="genre">
+                  <i className="fa fa-tag"></i>
+                  <h3>Post-Slow-Noise-Core-Wave</h3>
+                </div>
+              </section>
 
-            <article className="campaignList">
-              <h1>CAMPAIGNS</h1>
-              <div className="campBox">
-                <span>
-                  <i className="fa fa-map-marker"></i>
-                  <h3>Nashville</h3>
+              <section className="fundInfoBox">
+                <span className="fanSpan">
+                  <h3>Over 9000</h3>
+                  <p>True Fans</p>
                 </span>
-                <span>
-                  <i className="fa fa-calendar"></i>
-                  <h3>Brovember 11th</h3>
+                <span className="fundSpan">
+                  <h3>£12</h3>
+                  <p>pledged of £1000</p>
                 </span>
-                <span>
-                  <i className="fa fa-ticket"></i>
-                  <h3>$6 - General Admission</h3>
+                <span className="daysSpan">
+                  <h3>11</h3>
+                  <p>days to go</p>
                 </span>
                 <button className="bringBut" name="Bring This Band"></button>
-              </div>
+              </section>
+
+              <article className="campaignList">
+                <h1>CAMPAIGNS</h1>
+                <div className="campBox">
+                  <span>
+                    <i className="fa fa-map-marker"></i>
+                    <h3>Nashville</h3>
+                  </span>
+                  <span>
+                    <i className="fa fa-calendar"></i>
+                    <h3>Brovember 11th</h3>
+                  </span>
+                  <span>
+                    <i className="fa fa-ticket"></i>
+                    <h3>$6 - General Admission</h3>
+                  </span>
+                  <button className="bringBut" name="Bring This Band"></button>
+                </div>
 
             </article>
           </article>
--- src/js/components/fund.js
+++ src/js/components/fund.js
@@ -3,9 +3,47 @@ import React from 'react'
 class Fund extends React.Component {
   render () {
     return(
-      <section>
-        <p>Fund Page</p>
-      </section>
+      <div>
+        <div className="showInfo">
+          <div className="campBox">
+            <span>
+              <i className="fa fa-map-marker"></i>
+              <h3>Nashville</h3>
+            </span>
+            <span>
+              <i className="fa fa-calendar"></i>
+              <h3>Brovember 11th</h3>
+            </span>
+            <span>
+              <i className="fa fa-ticket"></i>
+              <h3>$6 - General Admission</h3>
+            </span>
+          </div>
+
+          <div className="deadBox">
+            <span>Funding Deadline:</span>
+            <span>October 11th</span>
+          </div>
+        </div>
+
+        <div className="pledge">
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+          <span>$6 - General Admission</span>
+
+        <button name="Pledge"></button>
+        </div>
+      </div>
     )
   }
 }
