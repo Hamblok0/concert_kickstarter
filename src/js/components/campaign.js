06f71c9fffa4a4dd1213741b8e865f90be178555
Joseph Cooper <hambloko@gmail.com>
2016-01-03 19:30:27 -0600

    errthang broken

--- src/js/components/campaign.js
+++ src/js/components/campaign.js
@@ -3,36 +3,92 @@ import moment from 'moment';
 moment().format();
 
 
+
+
 class Campaign extends React.Component {
-  render () {
-    let id = this.props.bandId;
-    let date = this.props.concert.performance_date
-    let momentTime = moment(date).format('LL');
-    let price = "$" + this.props.concert.price;
-    let concertEdit;
-    if (this.props.bandId === this.props.thisBandId) {
-      concertEdit = <input className="concertEdit" type="button" value="Edit Campaign" onClick={this.editCampaign}></input>
-    }
+  constructor(props) {
+    super(props)
+
+    this.editCampaign = this.editCampaign.bind(this);
+    this.returnToPage = this.returnToPage.bind(this);
 
 
-    return (
+
+  }
+
+  editCampaign(e){
+    html = (
       <section className="border">
-        {concertEdit}
         <span>
           <i className="fa fa-map-marker"></i>
-          <h3>{this.props.concert.location}</h3>
+          <input type="text" className="concertLoc" placeholder="City" />
         </span>
         <span>
           <i className="fa fa-calendar"></i>
-          <h3>{momentTime}</h3>
+          <input type="date" ref="date" />
         </span>
         <span>
           <i className="fa fa-ticket"></i>
-          <i>{price}</i>
-          <h3> - General Admission</h3>
+          <input type="text" className="tix" />
         </span>
-        <a href={`#band/${id}/fund`}><input type="submit" className="bringBtn" value="pledge"></input></a>
+        <a href={`#band/${id}`}><input type="submit" className="saveBtn" value="Save" onClick={this.returnToPage}></input></a>
       </section>
+    )
+  }
+
+  returnToPage(e){
+    html= (
+      <section className="border">
+           {concertEdit}
+           <span>
+             <i className="fa fa-map-marker"></i>
+             <h3>{this.props.concert.location}</h3>
+           </span>
+           <span>
+             <i className="fa fa-calendar"></i>
+             <h3>{momentTime}</h3>
+           </span>
+           <span>
+             <i className="fa fa-ticket"></i>
+             <i>{price}</i>
+             <h3> - General Admission</h3>
+           </span>
+           <a href={`#band/${id}/fund`}><input type="submit" className="bringBtn" value="pledge"></input></a>
+         </section>)
+
+  }
+
+  render () {
+    html;
+    id = this.props.bandId;
+    date = this.props.concert.performance_date
+    momentTime = moment(date).format('LL');
+    price = "$" + this.props.concert.price;
+    concertEdit;
+    if (this.props.bandId === this.props.thisBandId) {
+     concertEdit = <input className="concertEdit" type="button" value="Edit Campaign" onClick={this.editCampaign}></input>
+    }
+      html = (    <section className="border">
+          {concertEdit}
+          <span>
+            <i className="fa fa-map-marker"></i>
+            <h3>{this.props.concert.location}</h3>
+          </span>
+          <span>
+            <i className="fa fa-calendar"></i>
+            <h3>{momentTime}</h3>
+          </span>
+          <span>
+            <i className="fa fa-ticket"></i>
+            <i>{price}</i>
+            <h3> - General Admission</h3>
+          </span>
+          <a href={`#band/${id}/fund`}><input type="submit" className="bringBtn" value="pledge"></input></a>
+        </section>);
+
+
+    return (
+        {html}
     )
   }
 }
