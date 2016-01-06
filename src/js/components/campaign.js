4d7eb952e8aec122fec5f13889ac0d754a65fd5a
Mary Ellen Klein <maryellenklein@gmail.com>
2016-01-06 11:57:03 -0600

    fixing campaign bug

--- src/js/components/campaign.js
+++ src/js/components/campaign.js
@@ -16,7 +16,7 @@ class Campaign extends React.Component {
     let momentTime = moment(date).format('LL');
     let price = "$" + this.props.concert.price;
     if (this.props.bandId === this.props.thisBandId) {
-     concertEdit = <a href={`#band/${this.props.concert.id}/editcampaign`}><input className="concertEdit" type="button" value="Edit Campaign" ></input></a>
+     concertEdit = <a href={`#band/${this.props.concert.id}/editcampaign`}><input className="concertEdit" type="button" value="Edit" ></input></a>
   }
     return (
       <section className="border">
